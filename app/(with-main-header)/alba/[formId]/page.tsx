def112f65db73d961769963499113985f0ea94d9
heejin <dlwlrgkwk82@gmail.com>
2024-12-21 23:45:49 +0900

    :sparkles: feat: add alba detail second row

--- app/(with-main-header)/alba/[formId]/page.tsx
+++ app/(with-main-header)/alba/[formId]/page.tsx
@@ -23,10 +23,23 @@ const Page = async ({ params }: { params: Promise<{ formId: number }> }) => {
           <Chip value={mock.isPublic ? '공개' : '비공개'} />
           <Chip value={'모집중'} />
         </div>
-        <span className="text-gray-500 font-regular text-xs">
+        <span className="text-gray-500 font-regular text-xs lg:text-2lg">
           {formatFullDateTime(mock.createdAt)} 등록
         </span>
       </div>
+
+      <div>
+        <span className="text-black-400 font-semibold text-md lg:text-2xl relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1.5px] after:bg-black-400">
+          {mock.storeName}
+        </span>
+        <span className="text-gray-400 font-medium text-sm lg:text-xl ml-[10px] lg:ml-[16px]">
+          {mock.location} ・ 경력무관
+        </span>
+      </div>
+
+      <h2 className="font-semibold text-black-500 text-xl lg:text-3xl">
+        {mock.title}
+      </h2>
     </div>
   );
 };
