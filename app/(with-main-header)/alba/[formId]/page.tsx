6afb9673bbe62def036288e7c3cdedb479513d89
heejin <dlwlrgkwk82@gmail.com>
2024-12-21 16:19:00 +0900

    :sparkles: feat: add alba detail first row

--- app/(with-main-header)/alba/[formId]/page.tsx
+++ app/(with-main-header)/alba/[formId]/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Chip from '@/components/Chip';
+import { formatFullDateTime } from '@/utils/dateFormatter';
+
+const mock = {
+  isPublic: true,
+  createdAt: '2024-12-21T06:37:44.900Z',
+  storeName: '코드잇',
+  location: '서울시 종로구',
+  scrapCount: 8,
+  applyCount: 5,
+  title:
+    '코드잇 스터디카페 관리 (주말 오전) 모집합니다 서울 종로구 용산구 서대문',
+};
 
 const Page = async ({ params }: { params: Promise<{ formId: number }> }) => {
   const { formId } = await params;
+  console.log(formId); // TODO api 호출 후 제거
 
-  return <div>{formId}</div>;
+  return (
+    <div className="mt-[50px]">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-[8px]">
+          <Chip value={mock.isPublic ? '공개' : '비공개'} />
+          <Chip value={'모집중'} />
+        </div>
+        <span className="text-gray-500 font-regular text-xs">
+          {formatFullDateTime(mock.createdAt)} 등록
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ utils/dateFormatter.ts
@@ -0,0 +1,28 @@
+export function formatFullDateTime(targetDate: string | Date): string {
+  if (!targetDate) return '';
+
+  const date =
+    typeof targetDate === 'string' ? new Date(targetDate) : targetDate;
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const secs = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}.${month}.${day} ${hours}:${minutes}:${secs}`;
+}
+
+export function formatDate(targetDate: string | Date): string {
+  if (!targetDate) return '';
+
+  const date =
+    typeof targetDate === 'string' ? new Date(targetDate) : targetDate;
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}. ${month}. ${day}`;
+}
