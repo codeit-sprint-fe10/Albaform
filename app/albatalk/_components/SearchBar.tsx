b333ff319044dc7dbaa047f03b4f6c6c33684ddb
swim-kim <ksoo5386@naver.com>
2024-12-24 05:44:31 +0900

    ✨ feat: sort dropdown

--- app/albatalk/_components/SearchBar.tsx
+++ app/albatalk/_components/SearchBar.tsx
@@ -1,5 +1,7 @@
-import { ChangeEvent } from 'react';
+// SearchBar.tsx
+import React, { ChangeEvent } from 'react';
 import SearchIcon from '@/public/icons/search.svg';
+import SortDropdown from './SortDropdown';
 
 interface SearchBarProps {
   searchTerm: string;
@@ -26,17 +28,10 @@ const SearchBar: React.FC<SearchBarProps> = ({
     setNextCursor(0);
   };
 
-  const handleSortOrderChange = (
-    order: 'mostRecent' | 'mostLiked' | 'mostCommented',
-  ) => {
-    setSortOrder(order);
-    setCurrentPage(1);
-    setNextCursor(0);
-  };
   return (
     <div className="flex justify-center border-b-2">
       <div className="flex max-w-container-200 w-full h-28 items-center justify-between gap-4">
-        <div className="flex w-full max-w-6xl bg-background-200 border-none rounded-3xl px-6 py-4 justify-center items-center">
+        <div className="flex w-5/6 bg-background-200 border-none rounded-3xl px-6 py-4 justify-center items-center">
           <div className="flex items-center justify-center w-8 h-8">
             <SearchIcon className="text-gray-500" />
           </div>
@@ -48,26 +43,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
             className="w-full pl-2 bg-background-200 focus:outline-none"
           />
         </div>
-        <div className="flex gap-2">
-          <button
-            className={`px-4 py-2 ${sortOrder === 'mostRecent' ? 'bg-blue-500 text-white' : ''}`}
-            onClick={() => handleSortOrderChange('mostRecent')}
-          >
-            최신순
-          </button>
-          <button
-            className={`px-4 py-2 ${sortOrder === 'mostLiked' ? 'bg-blue-500 text-white' : ''}`}
-            onClick={() => handleSortOrderChange('mostLiked')}
-          >
-            좋아요순
-          </button>
-          <button
-            className={`px-4 py-2 ${sortOrder === 'mostCommented' ? 'bg-blue-500 text-white' : ''}`}
-            onClick={() => handleSortOrderChange('mostCommented')}
-          >
-            댓글 많은순
-          </button>
-        </div>
+        <SortDropdown sortOrder={sortOrder} setSortOrder={setSortOrder} />
       </div>
     </div>
   );
---
+++ app/albatalk/_components/SortDropdown.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+interface SortDropdownProps {
+  sortOrder: 'mostRecent' | 'mostLiked' | 'mostCommented';
+  setSortOrder: React.Dispatch<
+    React.SetStateAction<'mostRecent' | 'mostLiked' | 'mostCommented'>
+  >;
+}
+
+const SortDropdown: React.FC<SortDropdownProps> = ({
+  sortOrder,
+  setSortOrder,
+}) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
+
+  const handleSortOrderChange = (
+    selectedOrder: 'mostRecent' | 'mostLiked' | 'mostCommented',
+  ) => {
+    setSortOrder(selectedOrder);
+    setIsDropdownOpen(false);
+  };
+
+  return (
+    <div className="relative">
+      <div
+        className="px-4 py-2 bg-white rounded-md flex items-center justify-between gap-3 cursor-pointer"
+        onClick={handleDropdownToggle}
+      >
+        <span className="text-lg font-semibold">
+          {sortOrder === 'mostRecent'
+            ? '최신순'
+            : sortOrder === 'mostLiked'
+              ? '좋아요순'
+              : '댓글 많은순'}
+        </span>
+        <div
+          className={`w-2 h-2 border-t-2 border-r-2 border-gray-500 transform transition-transform ${
+            isDropdownOpen ? 'rotate-[135deg]' : '-rotate-45'
+          }`}
+        ></div>
+      </div>
+
+      {isDropdownOpen && (
+        <div className="absolute right-0 top-12 w-32 px-2 py-3 bg-gray-50 border border-gray-100 rounded-lg shadow-lg z-10">
+          <div
+            className={`w-full px-4 py-2 text-center text-lg rounded-lg font-semibold cursor-pointer hover:bg-orange-50 ${
+              sortOrder === 'mostRecent' ? 'bg-blue-500 text-white' : ''
+            }`}
+            onClick={() => handleSortOrderChange('mostRecent')}
+          >
+            최신순
+          </div>
+          <div
+            className={`w-full px-4 py-2 text-center text-lg rounded-lg font-semibold cursor-pointer hover:bg-orange-50 ${
+              sortOrder === 'mostLiked' ? 'bg-blue-500 text-white' : ''
+            }`}
+            onClick={() => handleSortOrderChange('mostLiked')}
+          >
+            좋아요순
+          </div>
+          <div
+            className={`w-full px-4 py-2 text-center text-lg rounded-lg font-semibold cursor-pointer hover:bg-orange-50 ${
+              sortOrder === 'mostCommented' ? 'bg-blue-500 text-white' : ''
+            }`}
+            onClick={() => handleSortOrderChange('mostCommented')}
+          >
+            댓글 많은순
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SortDropdown;
