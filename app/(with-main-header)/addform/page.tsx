93b757d490630c63e9282ee40c6583b6a5587094
najitwo <jitwona99@gmail.com>
2024-12-26 03:56:17 +0900

    ✨ feat: implement input component

--- app/(with-main-header)/addform/page.tsx
+++ app/(with-main-header)/addform/page.tsx
@@ -3,17 +3,30 @@
 import { useForm } from 'react-hook-form';
 import FileInput from './_components/FileInput';
 import LocationInput from './_components/LocationInput';
+import Input from './_components/Input';
+import Label from '@/components/Label';
 
 export interface FormValues {
+  title: string;
+  description: string;
   imageUrls: string[] | null;
   location: string;
 }
 
 const AddFormPage = () => {
-  const { setValue } = useForm<FormValues>();
+  const { setValue, register } = useForm<FormValues>();
 
   return (
     <>
+      <div>
+        <Label id="title" label="알바폼 제목" className="mb-4" required />
+        <Input
+          name="title"
+          placeholder="제목을 입력해주세요."
+          className="p-3.5 lg:py-4"
+          register={register('title')}
+        />
+      </div>
       <LocationInput setValue={setValue} />
       <FileInput setValue={setValue} />
     </>
