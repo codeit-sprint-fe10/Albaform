49dcccf1ca3bf35877a054efeb2f17046d642836
heejin <dlwlrgkwk82@gmail.com>
2024-12-31 10:25:22 +0900

    :sparkles: feat: add alba detail api

--- app/(with-main-header)/alba/[formId]/page.tsx
+++ app/(with-main-header)/alba/[formId]/page.tsx
@@ -8,6 +8,7 @@ import DescriptionSection from '@/app/(with-main-header)/alba/[formId]/_componen
 import FloatingActions from '@/app/(with-main-header)/alba/[formId]/_components/FloatingActions';
 import ApplicationActions from '@/app/(with-main-header)/alba/[formId]/_components/ApplicationActions';
 import Carousel from '@/app/(with-main-header)/alba/[formId]/_components/Carousel';
+import { getAlbaDetail } from '@/services/alba';
 
 const mock = {
   isPublic: true,
@@ -50,14 +51,14 @@ const AlbaFormIdPage = async ({
   params: Promise<{ formId: number }>;
 }) => {
   const { formId } = await params;
-  console.log(formId); // TODO api 호출 후 제거
+  const albaDetail = await getAlbaDetail(formId);
 
   return (
     <div>
       <div className="-mx-6 md:-mx-[72px] xl:mx-0 flex justify-center">
-        {mock.imageUrls?.length && (
+        {albaDetail.imageUrls?.length && (
           <div className="w-full">
-            <Carousel imageUrls={mock.imageUrls} />
+            <Carousel imageUrls={albaDetail.imageUrls} />
           </div>
         )}
       </div>
@@ -65,35 +66,35 @@ const AlbaFormIdPage = async ({
         <div className="flex flex-col lg:flex-row justify-between lg:gap-36">
           <div className="lg:basis-1/2">
             <div>
-              <AlertSection applyCount={mock.applyCount} />
+              <AlertSection applyCount={albaDetail.applyCount} />
               <div className="mt-8 md:mt-[80px]">
-                <SummarySection {...mock} />
+                <SummarySection {...albaDetail} />
               </div>
               <div className="mt-8 md:mt-10 lg:hidden">
-                <TermsSection {...mock} />
+                <TermsSection {...albaDetail} />
               </div>
               <div className="mt-8 lg:hidden">
-                <ContactSection {...mock} />
+                <ContactSection {...albaDetail} />
               </div>
               <div className="mt-8">
-                <DescriptionSection description={mock.description} />
+                <DescriptionSection description={albaDetail.description} />
               </div>
             </div>
             <div className="mt-8">
-              <Location location={mock.location} />
+              <Location location={albaDetail.location} />
             </div>
-            <FloatingActions isScrapped={mock.isScrapped} id={formId} />
+            <FloatingActions isScrapped={albaDetail.isScrapped} id={formId} />
           </div>
           <div className="lg:basis-1/2">
             <div className="lg:flex flex-col">
               <div className="hidden mt-8 md:mt-10 lg:block">
-                <TermsSection {...mock} />
+                <TermsSection {...albaDetail} />
               </div>
               <div className="hidden mt-8 lg:block">
-                <ContactSection {...mock} />
+                <ContactSection {...albaDetail} />
               </div>
               <div className="mt-8 lg:order-2">
-                <Requirements {...mock} />
+                <Requirements {...albaDetail} />
               </div>
               <div className="mt-10 mb-[30px] lg:order-1">
                 <ApplicationActions formId={formId} />
