c54344cd030eb3244da1f10000079bb46f11346b
swim-kim <ksoo5386@naver.com>
2024-12-31 21:21:24 +0900

    ♻️ refactor: Refactor like button to use optimistic update

--- app/(with-main-header)/albatalk/[talkId]/_components/LikeButton.tsx
+++ app/(with-main-header)/albatalk/[talkId]/_components/LikeButton.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import LikeIcon from '@/public/icons/like.svg';
 import { postLike, deleteLike } from '@/services/albatalk';
+import { useMutation } from '@tanstack/react-query';
 
 interface LikeButtonProps {
   postId: number;
@@ -12,42 +13,43 @@ interface LikeButtonProps {
 const LikeButton = ({ postId, isLiked, likeCount }: LikeButtonProps) => {
   const [liked, setLiked] = useState(isLiked);
   const [count, setCount] = useState(likeCount);
-  const [loading, setLoading] = useState(false);
 
-  const handleLikeToggle = async () => {
-    if (loading) return;
-    setLoading(true);
-
-    try {
-      const newLikeStatus = !liked;
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (newLikeStatus: boolean) => {
       if (newLikeStatus) {
         await postLike(postId);
       } else {
         await deleteLike(postId);
       }
+    },
 
+    onMutate: async (newLikeStatus) => {
+      const previousState = { liked, count };
       setLiked(newLikeStatus);
+      setCount((prevCount) => (newLikeStatus ? prevCount + 1 : prevCount - 1));
+      return previousState;
+    },
+    onError: (error, newLikeStatus, context) => {
+      if (context) {
+        setLiked(context.liked);
+        setCount(context.count);
+      }
+    },
+  });
 
-      const newCount = newLikeStatus ? count + 1 : count - 1;
-      setCount(newCount);
-    } catch (error) {
-      console.error('Failed to update like status:', error);
-    } finally {
-      setLoading(false);
-    }
+  const handleLikeToggle = async () => {
+    if (isPending) return;
+    const newLikeStatus = !liked;
+    mutate(newLikeStatus);
   };
 
   return (
     <div
-      className={`flex gap-1 items-center cursor-pointer ${
-        loading ? 'opacity-50 cursor-not-allowed' : ''
-      }`}
+      className={`flex gap-1 items-center cursor-pointer`}
       onClick={handleLikeToggle}
     >
       <LikeIcon
-        className={`w-6 h-6 lg:w-9 lg:h-9 ${
-          liked ? 'text-orange-300' : 'text-gray-100'
-        }`}
+        className={`w-6 h-6 lg:w-9 lg:h-9 ${liked ? 'text-orange-300' : 'text-gray-100'}`}
       />
       <div className="text-gray-500 text-xs md:text-md lg:text-lg font-regular">
         {count}
