591fe5e07b4c74ab2b2462b1dc5b2c1c1c013d3e
heejin <dlwlrgkwk82@gmail.com>
2025-01-03 01:35:06 +0900

    :recycle: refactor: pass resumeName on resume download

--- app/(with-main-header)/myapply/[formId]/_components/ApplicationDetail.tsx
+++ app/(with-main-header)/myapply/[formId]/_components/ApplicationDetail.tsx
@@ -16,7 +16,7 @@ const mock = {
   status: 'REJECTED',
   introduction:
     '코드잇 스터디 카페입니다. \n주말 토, 일 오픈업무 하실 분 구합니다.\n\n성실하게 일하실 분들만 지원 바랍니다.\n작성한 이력서(사진 부착)를 알바폼에 첨부해주시고, 아래와 같이 문자 보내주세요. \n근무 중 전화통화 불가합니다.\n\n 예) OOO입니다. __에 거주합니다. 알바폼 지원. \n\n이력서 검토 후 면접진행자에 한해 면접일정 개별 연락드리겠습니다. \n많은 지원 바랍니다.',
-  resumeName: 'resumeName',
+  resumeName: 'resumeName.pdf',
   resumeId: 266,
   experienceMonths: 13,
   phoneNumber: '01044448888',
@@ -33,7 +33,7 @@ const ApplicationDetail = ({ formId }: ApplicationDetailProps) => {
 
   const handleDownloadClick = async () => {
     try {
-      await getResumeFile(mock.resumeId);
+      await getResumeFile(mock.resumeId, mock.resumeName);
     } catch {
       alert('이력서 다운로드 실패!');
     }
--- services/resumeFile.ts
+++ services/resumeFile.ts
@@ -1,25 +1,24 @@
 import { instance } from './axiosInstance';
 
-export const getResumeFile = async (resumeId: number) => {
+export const getResumeFile = async (resumeId: number, resumeName: string) => {
   const response = await instance.get<Blob>(`/${resumeId}/download`, {
     responseType: 'blob',
   });
 
   // 문제점: 파일명을 받아와야하는데 헤더에 정보가 없음
-  const contentDisposition = response.headers['content-disposition'] as string;
-  console.log('contentDisposition', contentDisposition);
-  let filename = 'resume.pdf';
-  if (contentDisposition) {
-    const matches = /filename="([^"]+)"/.exec(contentDisposition);
-    if (matches && matches[1]) {
-      filename = matches[1];
-    }
-  }
+  // const contentDisposition = response.headers['content-disposition'] as string;
+  // let filename = 'resume.pdf';
+  // if (contentDisposition) {
+  //   const matches = /filename="([^"]+)"/.exec(contentDisposition);
+  //   if (matches && matches[1]) {
+  //     filename = matches[1];
+  //   }
+  // }
 
   const url = window.URL.createObjectURL(response.data);
   const link = document.createElement('a');
   link.href = url;
-  link.setAttribute('download', filename);
+  link.setAttribute('download', resumeName);
   document.body.appendChild(link);
   link.click();
 
