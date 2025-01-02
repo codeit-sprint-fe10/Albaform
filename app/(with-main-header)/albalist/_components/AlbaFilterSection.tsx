01bb2f617804376db57f755cbe161dd01b1d7621
jsh1147 <jsh1147@naver.com>
2025-01-02 21:16:08 +0900

    ✨ feat: add form list filter

---
+++ app/(with-main-header)/albalist/_components/AlbaFilterSection.tsx
@@ -0,0 +1,89 @@
+import { Dispatch, FormEvent, SetStateAction } from 'react';
+import { FilterProps } from '../page';
+import { FormOrderByEnglish } from '@/types/form';
+import Dropdown, { OptionProps } from './Dropdown';
+import SearchInput from './SearchInput';
+import {
+  FORM_ORDERBY_OPTIONS,
+  PUBLIC_OPTIONS,
+  RECRUIT_OPTIONS,
+} from '@/constants/dropdown';
+
+interface AlbaFilterSectionProps {
+  setFilter: Dispatch<SetStateAction<FilterProps>>;
+  setIsPublic: Dispatch<SetStateAction<boolean | undefined>>;
+}
+
+const AlbaFilterSection = ({
+  setFilter,
+  setIsPublic,
+}: AlbaFilterSectionProps) => {
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  const handleInputUpdate = (value: string) => {
+    const keyword = value ? value : undefined;
+    setFilter((prev) => ({ ...prev, keyword }));
+  };
+
+  const handleIsPublicSelect = (option: OptionProps) => {
+    setIsPublic(option.key as boolean | undefined);
+  };
+
+  const handleIsRecruitingSelect = (option: OptionProps) => {
+    setFilter((prev) => ({
+      ...prev,
+      isRecruiting: option.key as boolean | undefined,
+    }));
+  };
+
+  const handleOrderBySelect = (option: OptionProps) => {
+    setFilter((prev) => ({
+      ...prev,
+      orderBy: option.key as FormOrderByEnglish,
+    }));
+  };
+
+  return (
+    <form
+      onSubmit={handleFormSubmit}
+      className="fixed top-[50px] z-50 md:top-[69px] lg:top-[92px] left-0 w-full bg-gray-50"
+    >
+      <div
+        className={
+          'px-6 md:px-[72px] lg:px-[max(72px,calc((100%-1456px)/2))] ' +
+          'border-b border-line-100'
+        }
+      >
+        <SearchInput onUpdate={handleInputUpdate} />
+      </div>
+      <div
+        className={
+          'flex items-center justify-between w-full h-14 lg:h-20 px-6 ' +
+          'md:px-[72px] lg:px-[max(72px,calc((100%-1456px)/2))] border-b border-line-100'
+        }
+      >
+        <div className="flex items-center gap-3 lg:gap-4">
+          <Dropdown
+            type="filter"
+            options={PUBLIC_OPTIONS}
+            onSelect={handleIsPublicSelect}
+          />
+          <Dropdown
+            type="filter"
+            options={RECRUIT_OPTIONS}
+            onSelect={handleIsRecruitingSelect}
+          />
+        </div>
+        <Dropdown
+          type="sort"
+          options={FORM_ORDERBY_OPTIONS}
+          onSelect={handleOrderBySelect}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default AlbaFilterSection;
---
+++ app/(with-main-header)/albalist/_components/Dropdown.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import { FocusEvent, useRef, useState } from 'react';
+import ArrowIcon from '@/public/icons/chevron-down.svg';
+
+export interface OptionProps {
+  key: string | boolean | undefined;
+  label: string;
+}
+
+interface DropdownProps {
+  type: 'filter' | 'sort';
+  options: OptionProps[];
+  onSelect: (option: OptionProps) => void;
+}
+
+const Dropdown = ({ type, options, onSelect }: DropdownProps) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+  const handleDropdownBlur = (event: FocusEvent<HTMLDivElement>) => {
+    if (!dropdownRef.current?.contains(event.relatedTarget)) setIsOpen(false);
+  };
+
+  const handleSelectClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleOptionClick = (option: OptionProps) => {
+    setSelectedOption(option);
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  const divStyle = {
+    filter: 'relative w-[88px] lg:w-32',
+    sort: 'relative w-24 lg:w-36',
+  };
+
+  const selectStyle = {
+    filter:
+      'flex items-center justify-between w-full h-[30px] lg:h-[42px] pl-3 pr-2 py-[6px] ' +
+      'rounded border text-xs lg:text-2lg lg:font-medium ' +
+      `${
+        selectedOption === options[0]
+          ? 'border-gray-100 bg-gray-50 text-black-100'
+          : 'border-orange-300 bg-orange-50 text-orange-300'
+      }`,
+    sort:
+      'flex items-center justify-end gap-[2px] lg:gap-1 w-full h-[30px] lg:h-[42px] ' +
+      'py-[6px] text-xs lg:text-lg font-semibold text-black-300',
+  };
+
+  const arrowColor = {
+    filter: selectedOption === options[0] ? '#c4c4c4' : '#FCC369',
+    sort: '#c4c4c4',
+  };
+
+  const ulStyle = {
+    filter:
+      'absolute top-[calc(100%+5px)] lg:top-[calc(100%+7px)] z-10 w-full ' +
+      'border rounded border-gray-100 overflow-hidden shadow-md',
+    sort:
+      'absolute top-[calc(100%+5px)] lg:top-[calc(100%+7px)] z-10 w-full ' +
+      'border rounded-lg border-line-100 bg-background-100 shadow-md',
+  };
+
+  const optionStyle = {
+    filter:
+      'w-full py-2 lg:py-3 bg-gray-50 hover:bg-orange-50 text-xs lg:text-2lg ' +
+      'lg:font-medium text-black-100 hover:text-orange-300 transition duration-200',
+    sort:
+      'w-full py-[5px] rounded-lg border-[3px] lg:border-[7px] border-gray-50 ' +
+      'bg-gray-50 hover:bg-orange-50 text-xs lg:text-lg font-medium hover:font-semibold ' +
+      'text-gray-400 hover:text-black-400 transition duration-200',
+  };
+
+  return (
+    <div
+      ref={dropdownRef}
+      onBlur={handleDropdownBlur}
+      className={divStyle[type]}
+    >
+      <button
+        type="button"
+        onClick={handleSelectClick}
+        className={selectStyle[type]}
+      >
+        {selectedOption.label}
+        <ArrowIcon
+          alt=""
+          width={16}
+          height={16}
+          color={arrowColor[type]}
+          className="lg:w-6 lg:h-6"
+        />
+      </button>
+      {isOpen && (
+        <ul className={ulStyle[type]}>
+          {options.map((option) => {
+            return (
+              <li key={`${option.key}`}>
+                <button
+                  type="button"
+                  onClick={() => handleOptionClick(option)}
+                  className={optionStyle[type]}
+                >
+                  {option.label}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
