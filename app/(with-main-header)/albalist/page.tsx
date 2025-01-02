fe316e7a44550ac15d6036f7df9f605738e5c60e
jsh1147 <jsh1147@naver.com>
2025-01-02 21:15:36 +0900

    ✨ feat: add form list page & FAB

--- app/(with-main-header)/albalist/page.tsx
+++ app/(with-main-header)/albalist/page.tsx
@@ -1,5 +1,36 @@
-const Page = () => {
-  return <div>albaList</div>;
+'use client';
+
+import { useState } from 'react';
+import { useUserStore } from '@/store/user';
+import { GetFormsParameters } from '@/types/form';
+import { UserRole } from '@/types/user';
+import AlbaFilterSection from './_components/AlbaFilterSection';
+import AlbaListSection from './_components/AlbaListSection';
+import WriteFAB from './_components/WriteFAB';
+
+export type FilterProps = Pick<
+  GetFormsParameters,
+  'orderBy' | 'keyword' | 'isRecruiting'
+>;
+
+const initialFilter: FilterProps = {
+  orderBy: 'mostRecent',
+  keyword: undefined,
+  isRecruiting: undefined,
 };
 
-export default Page;
+const AlbaListPage = () => {
+  const user = useUserStore((state) => state.user);
+  const [filter, setFilter] = useState<FilterProps>(initialFilter);
+  const [isPublic, setIsPublic] = useState<boolean>();
+
+  return (
+    <>
+      <AlbaFilterSection setFilter={setFilter} setIsPublic={setIsPublic} />
+      <AlbaListSection filter={filter} isPublic={isPublic} />
+      {user?.role === UserRole.owner && <WriteFAB />}
+    </>
+  );
+};
+
+export default AlbaListPage;
