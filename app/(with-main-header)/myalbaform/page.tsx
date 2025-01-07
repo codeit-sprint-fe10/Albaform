0ed2b0664a1dc7b6f5650865ba42f359aab31cea
najitwo <jitwona99@gmail.com>
2025-01-07 22:46:45 +0900

    ✨ feat: add write button

--- app/(with-main-header)/myalbaform/page.tsx
+++ app/(with-main-header)/myalbaform/page.tsx
@@ -1,9 +1,11 @@
 import MyAlbas from './_components/MyAlbas';
+import WriteButton from './_components/WriteButton';
 
 const Page = () => {
   return (
     <div>
       <MyAlbas />
+      <WriteButton />
     </div>
   );
 };
