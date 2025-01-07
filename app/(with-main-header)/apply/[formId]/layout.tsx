8daa0b734b73c34114e5f025c37b603c0a9ce166
jsh1147 <jsh1147@naver.com>
2025-01-08 02:39:15 +0900

    ✨ feat: add duplication apply limit

--- app/(with-main-header)/apply/[formId]/layout.tsx
+++ app/(with-main-header)/apply/[formId]/layout.tsx
@@ -5,15 +5,22 @@ import { useParams, useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { getAlbaDetail } from '@/services/alba';
 import Loader from './_components/Loader';
+import { getMyApplication } from '@/services/application';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { formId } = useParams();
   const { replace } = useRouter();
 
-  const { isLoading, error } = useQuery({
+  const { isLoading: isValidLoading, error } = useQuery({
     queryKey: ['forms', formId],
     queryFn: () => getAlbaDetail(Number(formId)),
-    retry: 0,
+    retry: 1,
+  });
+
+  const { isLoading: isDuplicateLoading, data } = useQuery({
+    queryKey: ['my-application', formId],
+    queryFn: () => getMyApplication({ formId: Number(formId) }),
+    retry: 1,
   });
 
   useEffect(() => {
@@ -23,7 +30,15 @@ const Layout = ({ children }: { children: ReactNode }) => {
     }
   }, [error, replace]);
 
-  if (isLoading) return <Loader className="mt-24 lg:mt-32" />;
+  useEffect(() => {
+    if (data?.id) {
+      window.alert('이미 지원한 알바폼입니다.');
+      replace('/albalist');
+    }
+  }, [data, replace]);
+
+  if (isValidLoading || isDuplicateLoading)
+    return <Loader className="mt-24 lg:mt-32" />;
   return <>{children}</>;
 };
 
