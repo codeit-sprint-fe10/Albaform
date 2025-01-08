677bddb07e95caf45fc72e69ae1047eebee6ac99
heejin <dlwlrgkwk82@gmail.com>
2025-01-09 01:35:25 +0900

    :sparkles: feat: add radioIcon

--- app/(with-main-header)/applications/[formId]/[applicationId]/_components/UpdateAlbaStatusModal.tsx
+++ app/(with-main-header)/applications/[formId]/[applicationId]/_components/UpdateAlbaStatusModal.tsx
@@ -1,5 +1,10 @@
+'use client';
+
 import Modal from '@/components/Modal';
 import { UseModalProps } from '@/types/useModal';
+import { useState } from 'react';
+import { applicationStatus } from '@/types/application';
+import RadioIcon from '@/app/(with-main-header)/applications/[formId]/[applicationId]/_components/RadioIcon';
 
 interface UpdateAlbaStatusModalProps extends UseModalProps {
   applicationId: number;
@@ -10,6 +15,13 @@ const UpdateAlbaStatusModal = ({
   closeModal,
   applicationId,
 }: UpdateAlbaStatusModalProps) => {
+  const [selectedStatus, setSelectedStatus] =
+    useState<string>('INTERVIEW_PENDING');
+
+  const handleChange = (value: string) => {
+    setSelectedStatus(value);
+  };
+
   return (
     <Modal
       dialogRef={dialogRef}
@@ -18,6 +30,32 @@ const UpdateAlbaStatusModal = ({
       hasCloseButton={false}
     >
       <p>현재 진행상태를 알려주세요.</p>
+      <div>
+        {Object.entries(applicationStatus).map(([key, label]) => {
+          const isSelected = selectedStatus === key;
+
+          return (
+            <label
+              key={key}
+              className="flex justify-between items-center px-4 py-3 border-b border-gray-300 cursor-pointer"
+              onClick={() => handleChange(key)}
+            >
+              <span className="text-gray-800 text-sm md:text-base">
+                {label}
+              </span>
+              <input
+                type="radio"
+                name="albaStatus"
+                value={key}
+                checked={isSelected}
+                onChange={() => handleChange(key)}
+                className="hidden"
+              />
+              <RadioIcon isSelected={isSelected} />
+            </label>
+          );
+        })}
+      </div>
     </Modal>
   );
 };
