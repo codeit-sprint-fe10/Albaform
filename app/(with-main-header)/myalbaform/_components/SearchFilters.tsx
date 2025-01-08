7026380463d7661716fd9dc7666e1111df4ca3f9
najitwo <jitwona99@gmail.com>
2025-01-09 08:40:50 +0900

    🔥 chore: remove unused code

--- app/(with-main-header)/myalbaform/_components/SearchFilters.tsx
+++ app/(with-main-header)/myalbaform/_components/SearchFilters.tsx
@@ -10,10 +10,10 @@ const SearchFilters = () => {
     <div className="pt-[70px] md:pt-[86px] lg:pt-28 mb-[9px] md:mb-3.5 lg:mb-14">
       <div className="flex justify-between items-center py-3.5 lg:py-6">
         <div className="flex items-center gap-2.5 lg:gap-4">
-          <Dropdown name="isPublic" options={PUBLIC_OPTIONS}></Dropdown>
-          <Dropdown name="isRecruiting" options={RECRUIT_OPTIONS}></Dropdown>
+          <Dropdown name="isPublic" options={PUBLIC_OPTIONS} />
+          <Dropdown name="isRecruiting" options={RECRUIT_OPTIONS} />
         </div>
-        <Dropdown name="orderBy" options={ALBA_ORDERBY_OPTIONS}></Dropdown>
+        <Dropdown name="orderBy" options={ALBA_ORDERBY_OPTIONS} />
       </div>
     </div>
   );
