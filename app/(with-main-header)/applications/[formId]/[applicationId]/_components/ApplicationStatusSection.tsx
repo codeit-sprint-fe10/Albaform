cae28b5364ff173e0ef82b97ec7049207d94c68f
heejin <dlwlrgkwk82@gmail.com>
2025-01-08 19:37:23 +0900

    :sparkles: feat: add tooltip close action and animation

--- app/(with-main-header)/applications/[formId]/[applicationId]/_components/ApplicationStatusSection.tsx
+++ app/(with-main-header)/applications/[formId]/[applicationId]/_components/ApplicationStatusSection.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { Application, applicationStatus } from '@/types/application';
 import { formatDateTimeWithLetters } from '@/utils/dateFormatter';
 import InfoIcon from '@/public/icons/info.svg';
 import CloseIcon from '@/public/icons/x-thin.svg';
+import { useState } from 'react';
 
 type ApplicationStatusSectionProps = Pick<Application, 'createdAt' | 'status'>;
 
@@ -9,6 +12,8 @@ const ApplicationStatusSection = ({
   createdAt,
   status,
 }: ApplicationStatusSectionProps) => {
+  const [showTooltip, setShowTooltip] = useState(true);
+
   return (
     <>
       <section className="font-regular text-md text-black-400 lg:text-xl lg:p-6 lg:bg-background-100 lg:rounded-lg lg:border lg:border-line-100">
@@ -21,10 +26,16 @@ const ApplicationStatusSection = ({
           <span>{applicationStatus[status]}</span>
         </p>
       </section>
-      <div className="relative px-3 lg:px-6 py-2 lg:py-4 w-72 lg:w-[476px] bg-blue-200 flex items-center gap-2 rounded-xl font-semibold text-gray-50 text-2sm lg:text-xl before:content-[''] before:absolute before:bottom-full before:left-1/4 before:-translate-x-1/2 before:border-[10px] before:border-solid before:border-b-blue-200 before:border-gray-50">
+      <div
+        className={`relative px-3 lg:px-6 py-2 lg:py-4 w-72 lg:w-[476px] bg-blue-200 flex items-center gap-2 rounded-xl font-semibold text-gray-50 text-2sm lg:text-xl before:content-[''] before:absolute before:bottom-full before:left-1/4 before:-translate-x-1/2 before:border-8 before:border-solid before:border-b-blue-200 before:border-gray-50 transition-opacity duration-500 ease-out transform ${
+          showTooltip ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+      >
         <InfoIcon className="w-6 h-6 lg:w-9 lg:h-9" />
         <p className="flex-1">알바폼 현재 진행상태를 변경할 수 있어요!</p>
-        <CloseIcon className="w-6 h-6 lg:w-9 lg:h-9" />
+        <button aria-label="닫기" onClick={() => setShowTooltip(false)}>
+          <CloseIcon className="w-6 h-6 lg:w-9 lg:h-9" />
+        </button>
       </div>
     </>
   );
