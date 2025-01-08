57490e536cd8f55156d3e0c846f6432dcd3ab336
heejin <dlwlrgkwk82@gmail.com>
2025-01-09 02:00:51 +0900

    :sparkles: feat: add update application status button

--- app/(with-main-header)/applications/[formId]/[applicationId]/_components/UpdateAlbaStatusModal.tsx
+++ app/(with-main-header)/applications/[formId]/[applicationId]/_components/UpdateAlbaStatusModal.tsx
@@ -5,6 +5,7 @@ import { UseModalProps } from '@/types/useModal';
 import { useState } from 'react';
 import { applicationStatus } from '@/types/application';
 import RadioIcon from '@/app/(with-main-header)/applications/[formId]/[applicationId]/_components/RadioIcon';
+import Button from '@/components/Button';
 
 interface UpdateAlbaStatusModalProps extends UseModalProps {
   applicationId: number;
@@ -29,20 +30,19 @@ const UpdateAlbaStatusModal = ({
       title="진행상태 선택"
       hasCloseButton={false}
     >
-      <p>현재 진행상태를 알려주세요.</p>
-      <div>
+      <p className="text-center text-gray-400 font-regular text-md lg:text-xl mt-2 mb-6 lg:mt-4 lg:mb-12">
+        현재 진행상태를 알려주세요.
+      </p>
+      <div className="flex flex-col gap-2 font-medium text-black-400 text-md lg:text-2lg">
         {Object.entries(applicationStatus).map(([key, label]) => {
           const isSelected = selectedStatus === key;
-
           return (
             <label
               key={key}
-              className="flex justify-between items-center px-4 py-3 border-b border-gray-300 cursor-pointer"
+              className={`p-3.5 flex justify-between items-center border rounded-lg cursor-pointer ${isSelected ? 'border-orange-300 bg-orange-50' : 'border-line-100'} `}
               onClick={() => handleChange(key)}
             >
-              <span className="text-gray-800 text-sm md:text-base">
-                {label}
-              </span>
+              <span>{label}</span>
               <input
                 type="radio"
                 name="albaStatus"
@@ -55,6 +55,16 @@ const UpdateAlbaStatusModal = ({
             </label>
           );
         })}
+      </div>
+      <div className="flex justify-between gap-1 mt-8">
+        <button
+          type="button"
+          className="bg-gray-100 rounded-lg font-semibold text-lg lg:text-xl text-gray-50 w-full"
+          onClick={closeModal}
+        >
+          취소
+        </button>
+        <Button content="선택하기" />
       </div>
     </Modal>
   );
