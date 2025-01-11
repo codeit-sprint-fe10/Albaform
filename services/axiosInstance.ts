38cc21f7b4ab2333f2ef430ef9820d5fbcf1f3f3
jsh1147 <jsh1147@naver.com>
2025-01-11 17:32:58 +0900

    🐛 fix: adapt Axios instance to RSC

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -1,10 +1,19 @@
 import axios, { AxiosError } from 'axios';
 import { postRefresh } from './auth';
 
-const BASE_URL = 'https://fe-project-albaform.vercel.app/10-4';
+const BE_BASE_URL = 'https://fe-project-albaform.vercel.app/10-4';
+const FE_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://www.albaform.store'
+    : 'http://localhost:3000';
 
 export const instance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BE_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export const FEInstance = axios.create({
+  baseURL: FE_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
@@ -12,9 +21,9 @@ instance.interceptors.request.use(async (config) => {
   if (config.url === '/auth/refresh') return config;
 
   try {
-    const auths = await axios
-      .get<Record<string, string | null>>('/api/auth')
-      .then((res) => res.data);
+    const auths = await FEInstance.get<Record<string, string | null>>(
+      `/api/auth`,
+    ).then((res) => res.data);
 
     if (Object.values(auths).some((auth) => !auth))
       throw new AxiosError('저장된 유저 정보가 없습니다.', '401');
@@ -23,7 +32,7 @@ instance.interceptors.request.use(async (config) => {
       refreshToken: auths.refreshToken!,
     });
 
-    await axios.patch('/api/auth', { accessToken });
+    await FEInstance.patch('/api/auth', { accessToken });
     config.headers['Authorization'] = `Bearer ${accessToken}`;
   } catch (error) {
     const e = error as AxiosError<{ message: string }>;
@@ -32,7 +41,7 @@ instance.interceptors.request.use(async (config) => {
     if (res) console.log(`[${e.status}:${res.config.url}] ${res.data.message}`);
     else console.log(`[${e.code}] ${e.message}`);
 
-    await axios.delete('/api/auth');
+    await FEInstance.delete('/api/auth');
   }
 
   return config;
