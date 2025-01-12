859cc005163304fbeb314d1d7e01a3268fc9f04b
swim-kim <ksoo5386@naver.com>
2025-01-13 03:12:33 +0900

    ✨ feat: separate private scrap component

--- app/(with-main-header)/mypage/_components/Empty.tsx
+++ app/(with-main-header)/mypage/_components/Empty.tsx
@@ -6,21 +6,20 @@ interface EmptyProps {
   type: 'comment' | 'post' | 'scrap';
 }
 
-const Empty = ({ type }: EmptyProps) => {
-  const contentMap = {
-    comment: {
-      message: '등록된 댓글이 없어요.\n댓글을 작성해 의견을 남겨보세요.',
-    },
-    post: {
-      message:
-        '등록한 게시물이 없어요.\n게시물을 등록하고 의견을 공유해보세요.',
-    },
-    scrap: {
-      message: '스크랩한 항목이 없어요.\n관심 있는 알바를 스크랩해보세요.',
-    },
-  };
+const CONTENT_MESSAGE = {
+  comment: {
+    message: '등록된 댓글이 없어요.\n댓글을 작성해 의견을 남겨보세요.',
+  },
+  post: {
+    message: '등록한 게시물이 없어요.\n게시물을 등록하고 의견을 공유해보세요.',
+  },
+  scrap: {
+    message: '스크랩한 항목이 없어요.\n관심 있는 알바를 스크랩해보세요.',
+  },
+};
 
-  const { message } = contentMap[type];
+const Empty = ({ type }: EmptyProps) => {
+  const { message } = CONTENT_MESSAGE[type];
 
   return (
     <div className="mt-20 flex flex-col gap-8 justify-center items-center">
--- app/(with-main-header)/mypage/_components/scraps/ScrapCard.tsx
+++ app/(with-main-header)/mypage/_components/scraps/ScrapCard.tsx
@@ -7,6 +7,7 @@ import { calculateDDay } from '@/utils/dDayCalculator';
 import Badge from '@/components/Badge';
 import MenuDropdown from './MenuDropdown';
 import Link from 'next/link';
+import Private from './Private';
 
 const ScrapCard = ({
   id,
@@ -63,19 +64,7 @@ const ScrapCard = ({
               imageUrls[0] ? 'object-cover' : 'object-none'
             }`}
           />
-          {!isPublic && (
-            <div className="absolute top-0 left-0 z-20 flex flex-col justify-center items-center w-full h-full backdrop-blur-sm bg-black-400/50 rounded-xl lg:rounded-2xl">
-              <Image
-                src="/icons/private.svg"
-                width={80}
-                height={80}
-                alt="비공개"
-              />
-              <div className="text-md text-gray-50">
-                비공개 처리된 알바폼이에요
-              </div>
-            </div>
-          )}
+          {!isPublic && <Private />}
 
           <div className="flex justify-between items-center mb-4 lg:mb-6">
             <div className="flex items-center gap-2">
