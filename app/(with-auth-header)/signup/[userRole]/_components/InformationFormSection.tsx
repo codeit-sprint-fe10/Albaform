f14c0c15df872042b26c7e0d225cb76d74564539
jsh1147 <jsh1147@naver.com>
2025-01-14 07:44:06 +0900

    ♻️ refactor: refactor FormField

--- app/(with-auth-header)/signup/[userRole]/_components/InformationFormSection.tsx
+++ app/(with-auth-header)/signup/[userRole]/_components/InformationFormSection.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import { patchMe } from '@/services/user';
 import { NICKNAME, PHONE_NUMBER, STORE_NAME, LOCATION } from '@/constants/form';
 import { UserRoleLowerCase } from '@/types/user';
-import { patchMe } from '@/services/user';
 import FormField from '../../../_components/FormField';
 import Button from '@/components/Button';
 
@@ -22,20 +23,15 @@ interface InformationFormSectionProps {
 }
 
 const InformationFormSection = ({ userRole }: InformationFormSectionProps) => {
+  const [isFetching, setIsFetching] = useState(false);
   const { replace } = useRouter();
-  const {
-    register,
-    setValue,
-    setError,
-    clearErrors,
-    handleSubmit,
-    formState: { isValid, errors },
-  } = useForm<InformationFormData>({ mode: 'onTouched' });
+  const methods = useForm<InformationFormData>({ mode: 'onTouched' });
 
   const InformationSubmit: SubmitHandler<InformationFormData> = async (
     data,
     event,
   ) => {
+    setIsFetching(true);
     event?.preventDefault();
 
     try {
@@ -46,110 +42,118 @@ const InformationFormSection = ({ userRole }: InformationFormSectionProps) => {
     } catch {
       window.alert('오류가 발생했습니다.\n확인 후 다시 시도해 주세요.');
     }
+    setIsFetching(false);
   };
 
   return (
     <section>
-      <form
-        method="post"
-        onSubmit={handleSubmit(InformationSubmit)}
-        className="flex flex-col"
-      >
-        <FormField
-          name="nickname"
-          label="닉네임"
-          placeholder={NICKNAME.message.placeholder}
-          register={register('nickname', {
-            required: { value: true, message: NICKNAME.message.required },
-            maxLength: {
-              value: NICKNAME.format.maxLength,
-              message: NICKNAME.message.maxLength,
-            },
-            pattern: {
-              value: NICKNAME.format.regExp,
-              message: NICKNAME.message.pattern,
-            },
-          })}
-          error={errors.nickname}
-        />
-        <FormField
-          name="phoneNumber"
-          label="전화번호"
-          placeholder={PHONE_NUMBER.message.placeholder}
-          register={register('phoneNumber', {
-            required: { value: true, message: PHONE_NUMBER.message.required },
-            minLength: {
-              value: PHONE_NUMBER.format.minLength,
-              message: PHONE_NUMBER.message.minLength,
-            },
-            maxLength: {
-              value: PHONE_NUMBER.format.maxLength,
-              message: PHONE_NUMBER.message.maxLength,
-            },
-            pattern: {
-              value: PHONE_NUMBER.format.regExp,
-              message: PHONE_NUMBER.message.pattern,
-            },
-          })}
-          error={errors.phoneNumber}
-        />
-        {userRole === 'owner' && (
-          <>
-            <FormField
-              name="storeName"
-              label="가게 이름"
-              placeholder={STORE_NAME.message.placeholder}
-              register={register('storeName', {
-                required: { value: true, message: STORE_NAME.message.required },
-                maxLength: {
-                  value: STORE_NAME.format.maxLength,
-                  message: STORE_NAME.message.maxLength,
-                },
-              })}
-              error={errors.storeName}
-            />
-            <FormField
-              name="storePhoneNumber"
-              label="가게 전화번호"
-              placeholder={PHONE_NUMBER.message.placeholder}
-              register={register('storePhoneNumber', {
-                required: {
-                  value: true,
-                  message: PHONE_NUMBER.message.required,
-                },
-                minLength: {
-                  value: PHONE_NUMBER.format.minLength,
-                  message: PHONE_NUMBER.message.minLength,
-                },
-                maxLength: {
-                  value: PHONE_NUMBER.format.maxLength,
-                  message: PHONE_NUMBER.message.maxLength,
-                },
-                pattern: {
-                  value: PHONE_NUMBER.format.regExp,
-                  message: PHONE_NUMBER.message.pattern,
-                },
-              })}
-              error={errors.storePhoneNumber}
-            />
-            <FormField
-              name="location"
-              label="가게 위치"
-              placeholder={LOCATION.message.placeholder}
-              error={errors.location}
-              setValue={setValue}
-              setError={setError}
-              clearErrors={clearErrors}
-            />
-          </>
-        )}
-        <Button
-          type="submit"
-          content="다음"
-          disabled={!isValid}
-          className="mt-8 lg:mt-12"
-        ></Button>
-      </form>
+      <FormProvider {...methods}>
+        <form
+          method="post"
+          onSubmit={methods.handleSubmit(InformationSubmit)}
+          className="flex flex-col"
+        >
+          <FormField
+            name="nickname"
+            label="닉네임"
+            placeholder={NICKNAME.message.placeholder}
+            register={methods.register('nickname', {
+              required: { value: true, message: NICKNAME.message.required },
+              maxLength: {
+                value: NICKNAME.format.maxLength,
+                message: NICKNAME.message.maxLength,
+              },
+              pattern: {
+                value: NICKNAME.format.regExp,
+                message: NICKNAME.message.pattern,
+              },
+            })}
+            error={methods.formState.errors.nickname}
+            design="outlined"
+          />
+          <FormField
+            name="phoneNumber"
+            label="전화번호"
+            placeholder={PHONE_NUMBER.message.placeholder}
+            register={methods.register('phoneNumber', {
+              required: { value: true, message: PHONE_NUMBER.message.required },
+              minLength: {
+                value: PHONE_NUMBER.format.minLength,
+                message: PHONE_NUMBER.message.minLength,
+              },
+              maxLength: {
+                value: PHONE_NUMBER.format.maxLength,
+                message: PHONE_NUMBER.message.maxLength,
+              },
+              pattern: {
+                value: PHONE_NUMBER.format.regExp,
+                message: PHONE_NUMBER.message.pattern,
+              },
+            })}
+            error={methods.formState.errors.phoneNumber}
+            design="outlined"
+          />
+          {userRole === 'owner' && (
+            <>
+              <FormField
+                name="storeName"
+                label="가게 이름"
+                placeholder={STORE_NAME.message.placeholder}
+                register={methods.register('storeName', {
+                  required: {
+                    value: true,
+                    message: STORE_NAME.message.required,
+                  },
+                  maxLength: {
+                    value: STORE_NAME.format.maxLength,
+                    message: STORE_NAME.message.maxLength,
+                  },
+                })}
+                error={methods.formState.errors.storeName}
+                design="outlined"
+              />
+              <FormField
+                name="storePhoneNumber"
+                label="가게 전화번호"
+                placeholder={PHONE_NUMBER.message.placeholder}
+                register={methods.register('storePhoneNumber', {
+                  required: {
+                    value: true,
+                    message: PHONE_NUMBER.message.required,
+                  },
+                  minLength: {
+                    value: PHONE_NUMBER.format.minLength,
+                    message: PHONE_NUMBER.message.minLength,
+                  },
+                  maxLength: {
+                    value: PHONE_NUMBER.format.maxLength,
+                    message: PHONE_NUMBER.message.maxLength,
+                  },
+                  pattern: {
+                    value: PHONE_NUMBER.format.regExp,
+                    message: PHONE_NUMBER.message.pattern,
+                  },
+                })}
+                error={methods.formState.errors.storePhoneNumber}
+                design="outlined"
+              />
+              <FormField
+                name="location"
+                label="가게 위치"
+                placeholder={LOCATION.message.placeholder}
+                error={methods.formState.errors.location}
+                design="outlined"
+              />
+            </>
+          )}
+          <Button
+            type="submit"
+            content="시작하기"
+            disabled={!methods.formState.isValid || isFetching}
+            className="mt-8 lg:mt-12"
+          ></Button>
+        </form>
+      </FormProvider>
     </section>
   );
 };
--- types/form.ts
+++ types/form.ts
@@ -2,29 +2,10 @@ import { User } from './user';
 import { Alba } from './alba';
 import { Application } from './application';
 
-export type Field = User & Alba & Application;
+type Field = User & Alba & Application & { passwordConfirmation: string };
+type CommonField = Omit<Field, 'location' | 'resumeId' | 'resumeName'>;
+type CustomField = Pick<Field, 'location' | 'resumeId' | 'resumeName'>;
 
 export type FieldName = keyof Field;
-
-export type CommonFieldName = keyof Omit<
-  Field,
-  'location' | 'resumeId' | 'resumeName'
->;
-
-export type CustomFieldName = keyof Pick<
-  Field,
-  'location' | 'resumeId' | 'resumeName'
->;
-
-export type CustomSetValue<N extends FieldName> = (
-  name: N,
-  value: string,
-  options?: { shouldDirty: boolean },
-) => void;
-
-export type CustomSetError<N extends FieldName> = (
-  name: N,
-  error: { type: string; message: string },
-) => void;
-
-export type CustomClearErrors<N extends FieldName> = (name: N) => void;
+export type CommonFieldName = keyof CommonField;
+export type CustomFieldName = keyof CustomField;
