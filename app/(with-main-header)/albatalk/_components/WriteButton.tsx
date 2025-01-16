1cdb14eaf4f5eb05fd5e965ad49c29a083e3da23
swim-kim <ksoo5386@naver.com>
2025-01-16 14:33:44 +0900

    💄 style: change dropdown chevron

--- app/(with-main-header)/albatalk/_components/WriteButton.tsx
+++ app/(with-main-header)/albatalk/_components/WriteButton.tsx
@@ -8,7 +8,7 @@ const WriteButton = () => {
       <aside className="w-[54px] h-[54px] lg:w-[64px] lg:h-[64px] fixed bottom-10 right-10 flex flex-col  bg-orange-300 rounded-full justify-center items-center shadow-md shadow-gray-300">
         <button aria-label="작성하기">
           <div className="relative w-6 h-6 lg:w-9 lg:h-9">
-            <EditIcon fill="white" />
+            <EditIcon className="text-gray-50" />
           </div>
         </button>
       </aside>
