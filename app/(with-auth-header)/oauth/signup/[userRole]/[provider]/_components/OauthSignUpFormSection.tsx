90469d69ea30681a79e84fcdde85a69b2ef9e6ba
jsh1147 <jsh1147@naver.com>
2025-01-21 07:22:59 +0900

    ✨ feat: add terms of service

--- app/(with-auth-header)/oauth/signup/[userRole]/[provider]/_components/OauthSignUpFormSection.tsx
+++ app/(with-auth-header)/oauth/signup/[userRole]/[provider]/_components/OauthSignUpFormSection.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
@@ -200,6 +201,16 @@ const OauthSignUpFormSection = ({
             disabled={!methods.formState.isValid || isPending}
             className="mt-8 lg:mt-12"
           ></Button>
+          <span className="mt-4 lg:mt-6 text-center text-xs lg:text-lg text-black-100">
+            {'가입 시 '}
+            <Link
+              href="https://sprint.codeit.kr"
+              className="text-orange-300 hover:text-orange-200 font-semibold underline underline-offset-2 transition duration-200"
+            >
+              이용약관
+            </Link>
+            에 동의한 것으로 간주됩니다.
+          </span>
         </form>
       </FormProvider>
     </section>
--- app/(with-auth-header)/oauth/signup/[userRole]/[provider]/_components/OauthSignUpHeadSection.tsx
+++ app/(with-auth-header)/oauth/signup/[userRole]/[provider]/_components/OauthSignUpHeadSection.tsx
@@ -1,3 +1,4 @@
+import RoleNav from '@/components/RoleNav';
 import { UserRoleLowerCase } from '@/types/user';
 
 interface OauthSignUpHeadSectionProps {
@@ -9,8 +10,9 @@ const OauthSignUpHeadSection = ({ userRole }: OauthSignUpHeadSectionProps) => {
 
   return (
     <section className="flex flex-col items-center text-center">
+      <RoleNav isHeader={false} />
       <h1 className="mt-10 md:mt-0 mb-4 md:mb-6 lg:mb-8 text-2xl lg:text-3xl font-semibold">
-        회원가입
+        간편 회원가입
       </h1>
       <p className="text-xs md:text-md lg:text-xl text-black-100">
         추가 정보를 입력하여 회원가입을 완료해주세요.
